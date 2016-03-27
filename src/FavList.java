ce5ed258c9c49905d9cb0b2ab3d7b4cebcd0c181
ToddRMIT <s3435660@student.rmit.edu.au>
2016-03-27 16:59:32 +1100

    Added FavList.java

---
+++ src/FavList.java
@@ -0,0 +1,72 @@
+public class FavList{
+	
+	
+
+	private static class FavNode{
+		private Site site;
+		private String currentTemp;
+		private FavNode nextFav;
+
+		public FavNode( Site site, String currentTemp ){
+			this.site = site;
+			this.currentTemp = currentTemp;
+			nextFav = null;
+		}
+		public String getName(){ return site.getName(); }
+		public String getURL(){ return site.getURL(); }
+		public String getCurrentTemp(){ return currentTemp; }
+		public void setCurrentTemp( String currentTemp ){ this.currentTemp = currentTemp; }
+		public FavNode nextFav(){ return nextFav; }
+		public void setNext( FavNode nextFav ){ this.nextFav = nextFav; }
+	}
+
+	
+
+	private FavNode favHead;
+	private int favLength;
+
+	public FavList(){
+		favHead = null;
+		favLength = 0;
+	}
+	public int getLength(){ return favLength; }
+
+
+
+	public void addFav( Site site, String currentTemp ){
+		FavNode newFav = new FavNode( site, currentTemp );
+		FavNode thisFav = favHead;
+		if( favLength == 0 ){
+			favHead = newFav;
+			favLength += 1;
+			return;
+		}
+		if( newFav.getName().compareTo( thisFav.getName() ) < 0 ){
+			newFav.setNext( thisFav );
+			favHead = newFav;
+			favLength += 1;
+			return;
+		}
+		while( thisFav.nextFav() != null ){
+			if( newFav.getName().compareTo( thisFav.nextFav().getName() ) < 0 ) break;
+		}
+		newFav.setNext( thisFav.nextFav() );
+		thisFav.setNext( newFav );
+		favLength += 1;
+		return;
+	}
+
+
+
+	public void printFav(){
+		FavNode thisFav = favHead;
+		while( thisFav != null ){
+			System.out.println( thisFav.getName() + " - " + thisFav.getURL() + " - " + thisFav.getCurrentTemp() );
+			thisFav = thisFav.nextFav();
+		}
+		return;
+	}
+
+
+
+}
--- src/WeatherApp.java
+++ src/WeatherApp.java
@@ -8,12 +8,20 @@ import java.net.URL;
 public class WeatherApp {
 
 	public static void main(String[] args) throws IOException{
+		FavList favList = new FavList();
 		SiteList siteList = new SiteList();
 		Utility.FetchSites( siteList );
 
 		/* FOR TESTING */
 		siteList.printSites();
 		System.out.println("Number of sites: " + siteList.getLength());
+
+		favList.addFav( new Site( "wodonga", "wodongaURL" ), "23" );
+		favList.addFav( new Site( "albury", "alburyURL" ), "25" );
+		favList.addFav( new Site( "lavington", "lavingtonURL" ), "24" );
+
+		favList.printFav();
+
 	}
 
 }
