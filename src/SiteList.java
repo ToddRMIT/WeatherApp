5ba3d4bfc004a72b5b046bed0b18d6977d367ea1
ToddRMIT <s3435660@student.rmit.edu.au>
2016-03-27 19:18:33 +1100

    Converted tabs to spaces

--- src/SiteList.java
+++ src/SiteList.java
@@ -1,79 +1,79 @@
 
 
 public class SiteList{
-	
-	private static class SiteNode{
-		private Site thisSite;
-		private SiteNode nextSite;
+    
+    private static class SiteNode{
+        private Site thisSite;
+        private SiteNode nextSite;
 
-		public SiteNode( Site thisSite ){
-			this.thisSite = thisSite;
-			nextSite = null;
-		}
-		public void setNext( SiteNode nextSite ){
-			this.nextSite = nextSite;
-		}
-		public SiteNode getNext(){
-			return nextSite;
-		}
-		public String getName(){
-			return thisSite.getName();
-		}
-		public String getURL(){
-			return thisSite.getURL();
-		}
-	}
+        public SiteNode( Site thisSite ){
+            this.thisSite = thisSite;
+            nextSite = null;
+        }
+        public void setNext( SiteNode nextSite ){
+            this.nextSite = nextSite;
+        }
+        public SiteNode getNext(){
+            return nextSite;
+        }
+        public String getName(){
+            return thisSite.getName();
+        }
+        public String getURL(){
+            return thisSite.getURL();
+        }
+    }
 
 
-	private static SiteNode firstSite;
-	private static int length;
+    private static SiteNode firstSite;
+    private static int length;
 
-	public SiteList(){
-		firstSite = null;
-		length = 0;
-	}
-	public int getLength(){
-		return length;
-	}
+    public SiteList(){
+        firstSite = null;
+        length = 0;
+    }
+    public int getLength(){
+        return length;
+    }
 
-	// Insert site in ascending alphabetical order
-	public static void insertSite( Site thisSite ){
-		SiteNode newNode = new SiteNode( thisSite );
-		SiteNode thisNode = firstSite;
-		// Check if only site
-		if( length == 0 ){
-			firstSite = newNode;
-			++length;
-			return;
-		}
-		// Check if new site should be before first site
-		if( newNode.getName().compareTo( firstSite.getName() ) < 0 ){
-			newNode.setNext( thisNode );
-			firstSite = newNode;
-			++length;
-			return;
-		}
-		// Find new sites place in the list
-		while( thisNode.getNext() != null ){
-			if( newNode.getName().compareTo( thisNode.getNext().getName() ) <= 0 ) break;
-			thisNode = thisNode.getNext();
-		}
-		// Check if duplicate
-		if( thisNode.getNext() == null || newNode.getName().compareTo( thisNode.getNext().getName() ) != 0 ){
-			newNode.setNext( thisNode.getNext() );
-			thisNode.setNext( newNode );
-			++length;	
-		}
-		return;
-	}
+    // Insert site in ascending alphabetical order
+    public static void insertSite( Site thisSite ){
+        SiteNode newNode = new SiteNode( thisSite );
+        SiteNode thisNode = firstSite;
+        // Check if only site
+        if( length == 0 ){
+            firstSite = newNode;
+            ++length;
+            return;
+        }
+        // Check if new site should be before first site
+        if( newNode.getName().compareTo( firstSite.getName() ) < 0 ){
+            newNode.setNext( thisNode );
+            firstSite = newNode;
+            ++length;
+            return;
+        }
+        // Find new sites place in the list
+        while( thisNode.getNext() != null ){
+            if( newNode.getName().compareTo( thisNode.getNext().getName() ) <= 0 ) break;
+            thisNode = thisNode.getNext();
+        }
+        // Check if duplicate
+        if( thisNode.getNext() == null || newNode.getName().compareTo( thisNode.getNext().getName() ) != 0 ){
+            newNode.setNext( thisNode.getNext() );
+            thisNode.setNext( newNode );
+            ++length;   
+        }
+        return;
+    }
 
-	public static void printSites(){
-		SiteNode index = firstSite;
-		while( index != null ){
-			System.out.println( index.getName() + " " + index.getURL() );
-			index = index.getNext();
-		}
-		return;
-	}
+    public static void printSites(){
+        SiteNode index = firstSite;
+        while( index != null ){
+            System.out.println( index.getName() + " " + index.getURL() );
+            index = index.getNext();
+        }
+        return;
+    }
 
 }
--- src/WeatherApp.java
+++ src/WeatherApp.java
@@ -7,29 +7,29 @@ import java.net.URL;
 
 public class WeatherApp {
 
-	public static void main(String[] args) throws IOException{
-		FavList favList = new FavList();
-		SiteList siteList = new SiteList();
-		Utility.FetchSites( siteList );
+    public static void main(String[] args) throws IOException{
+        FavList favList = new FavList();
+        SiteList siteList = new SiteList();
+        Utility.FetchSites( siteList );
 
-		/* FOR TESTING */
-		siteList.printSites();
-		System.out.println("Number of sites: " + siteList.getLength());
+        /* FOR TESTING */
+        siteList.printSites();
+        System.out.println("Number of sites: " + siteList.getLength());
 
-		favList.add( new Site( "wodonga", "wodongaURL" ), "23" );
-		favList.add( new Site( "albury", "alburyURL" ), "25" );
-		favList.add( new Site( "lavington", "lavingtonURL" ), "24" );
+        favList.add( new Site( "wodonga", "wodongaURL" ), "23" );
+        favList.add( new Site( "albury", "alburyURL" ), "25" );
+        favList.add( new Site( "lavington", "lavingtonURL" ), "24" );
 
-		favList.print();
+        favList.print();
 
-		favList.remove( "wodonga" );
+        favList.remove( "wodonga" );
 
-		favList.print();
-		favList.save();
-		favList = new FavList();
-		favList.load();
-		favList.print();
+        favList.print();
+        favList.save();
+        favList = new FavList();
+        favList.load();
+        favList.print();
 
-	}
+    }
 
 }
