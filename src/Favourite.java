5097d3aca8fffd865e408402fe5fa5437143dc52
Todd <s3435660@student.rmit.edu.au>
2016-03-29 22:02:45 +1100

    Convert tabs to spaces

--- src/Favourite.java
+++ src/Favourite.java
@@ -1,14 +1,14 @@
 public class Favourite extends Site{
 
-		private double currentTemp;
+        private double currentTemp;
 
-		public Favourite( String name, String url, double currentTemp ){
-			super( name, url );
-			this.currentTemp = currentTemp;
-		}
+        public Favourite( String name, String url, double currentTemp ){
+            super( name, url );
+            this.currentTemp = currentTemp;
+        }
 
-		public double getTemp(){
-			return currentTemp;
-		}
+        public double getTemp(){
+            return currentTemp;
+        }
 
 }
