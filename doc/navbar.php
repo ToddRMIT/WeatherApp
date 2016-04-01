f7d77637fac6832fbe679d5e8f9590b75b4768cc
rmit-s3330458-luke-young <Young@Lukes-MacBook-Pro-2.local>
2016-04-01 13:13:26 +1100

    html user doc prelim
    - setup the document
    - placeholder text, images and links

---
+++ doc/navbar.php
@@ -0,0 +1,9 @@
+<ul>
+    <li><a href="#home">Home</a></li>
+    <li><a href="#installation">Installation</a></li>
+    <li><a href="#screenshots">Screenshots</a></li>
+    <li><a href="#code_snippets">Code Snippets</a></li>
+    <li><a href="#uml_diagrams">UML Diagrams</a></li>
+    <li><a href="#contact_us">Contact Us</a></li>
+    <li><a href="#about">About</a></li>
+</ul>
