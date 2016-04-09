71f00e82c4225ece7af87bf36666649a427205ac
Todd <s3435660@student.rmit.edu.au>
2016-04-10 01:07:23 +1000

    Main window now loads / saves X, Y co-ords

--- src/GuiHandler.java
+++ src/GuiHandler.java
@@ -1,11 +1,15 @@
-
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.swing.GroupLayout.Alignment;
 
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.stage.WindowEvent;
 import javafx.geometry.HPos;
 import javafx.geometry.Insets;
 import javafx.geometry.Orientation;
@@ -26,6 +30,7 @@ public class GuiHandler extends Application {
 
 	public static final String SITES_FILE = "sites.txt";
 	public static final String FAVOURITES_FILE = "favourites.txt";
+	public static final String MAIN_PREFERENCES_FILE = "prefs.txt";
 	
 	Stage window;
     static boolean listOpen;
@@ -40,6 +45,15 @@ public class GuiHandler extends Application {
 
 		window = primaryStage;
 		window.setTitle("Weather app");
+		
+		
+		
+		// Load preferences
+		// Currently returns an array of dimension 2
+		// [0]: window.X    [1]: window.Y
+		String prefs[] = loadPrefs();
+		
+		
 		
 		//Button creation
 		GridPane top = new GridPane();
@@ -126,19 +140,19 @@ public class GuiHandler extends Application {
 		}
 		
 		//Button Press event handler for the favourites buttons to open data window
-				for( int i = 0; i < list.length; ++i ) {
-					final int selected = i;
-					favButtons[i].setOnAction(new EventHandler<ActionEvent>() {
-						@Override public void handle(ActionEvent e) {
-							try {
-								listOpen = true;
-								GuiDataWindow.dataWindow(primaryStage, "Data Window");
-							} catch (IOException e1) {
-								e1.printStackTrace();
-							}	
-						}
-					});
+		for( int i = 0; i < list.length; ++i ) {
+			final int selected = i;
+			favButtons[i].setOnAction(new EventHandler<ActionEvent>() {
+				@Override public void handle(ActionEvent e) {
+					try {
+						listOpen = true;
+						GuiDataWindow.dataWindow(primaryStage, "Data Window");
+					} catch (IOException e1) {
+						e1.printStackTrace();
+					}	
 				}
+			});
+		}
 		
 		//Creates border and sets the layout for the elements
         BorderPane border = new BorderPane();
@@ -148,12 +162,69 @@ public class GuiHandler extends Application {
         //Creates the window and initiates the scene
         Scene scene = new Scene(border);
         window.setScene(scene);
+        window.setX( Double.parseDouble( prefs[0] ) );
+        window.setY( Double.parseDouble( prefs[1] ) );
+        window.setOnCloseRequest( new EventHandler<WindowEvent>(){ 
+        	@Override 
+        	public void handle(final WindowEvent e){
+        		savePrefs( window.getX(), window.getY() );
+        	}
+        });
         window.show();
+        
+        // Print the window co-ords to console
+        String windowX = Double.toString(window.getX());
+        String windowY = Double.toString(window.getY());
+        System.out.println("X: " + windowX + ", Y: " + windowY );
 	}
 	
 	//sets list windows open tracking value to false
 	public static void listClosed() {
 		listOpen = false;
 	}
+	
+	
+	
+	public static String[] loadPrefs(){
+		String prefs[] = new String[2];
+		BufferedReader file = null;
+		try{
+			file = new BufferedReader( new FileReader( MAIN_PREFERENCES_FILE ) );
+			String line;
+			int i = 0;
+			while( ( line = file.readLine() ) != null ){
+				prefs[i]=line;
+				++i;
+			}
+		}
+		catch (IOException e){
+			System.err.println( " Error: File " + MAIN_PREFERENCES_FILE + " not found." );
+			// File not found
+			// Falling back to sane defaults
+			prefs = new String[2];
+			prefs[0] = "100";
+			prefs[1] = "100";
+		}
+		return prefs;
+	}
+	
+	
+	
+	public static void savePrefs( Double X, Double Y ){
+		PrintWriter out = null;
+		try{
+			out = new PrintWriter( new FileWriter( MAIN_PREFERENCES_FILE ) );
+			out.println( Double.toString(X) );
+			out.println( Double.toString(Y) );
+		}
+		catch (IOException e){
+			System.err.println( " Error: File " + MAIN_PREFERENCES_FILE + " not found." );
+		}
+		finally{
+			if( out != null ) out.close();
+		}
+	}
+	
+	
 
 }
