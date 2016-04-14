3527d7446539fec675f7cd05f3baa3f395de5149
rmit-s3330458-luke-young <Young@Lukes-MacBook-Pro-2.local>
2016-04-14 12:19:58 +1000

    HTML and Java Style Update
    - Formatted WeatherApp.java and Utility.java in accordance with the
    Google Java Style guide
    - Fixed links in HTML documentation

--- src/WeatherApp.java
+++ src/WeatherApp.java
@@ -7,13 +7,10 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.Scene;
 
-
-
 public class WeatherApp {
 	
-	
     public static void main(String[] args) throws IOException {
-        GuiHandler guiHandler = new GuiHandler(  );
+        GuiHandler guiHandler = new GuiHandler();
         guiHandler.launch(GuiHandler.class,args);
     }
 
